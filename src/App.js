54ec3c428664b1f32e1776ce939ae8aab09bf7b5
kariants <kariantti.s@gmail.com>
2022-08-10 05:47:23 +0300

    react-hook-tests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef, useReducer } from "react";
+
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement"
+}
+
+function reducer(state, action) {
+
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 };
+  
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1 }
+
+    default:
+      return state;
+  }
+  
+}
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  //const [count, setCount] = useState(3);
+
+  function decrementCount() {
+    //setCount(prevCount => prevCount - 1);
+    dispatch({type: ACTIONS.DECREMENT});
+  }
+
+  function increaseCount() {
+    //setCount(prevCount => prevCount + 1);
+    dispatch({type: ACTIONS.INCREMENT});
+  }
+
+
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    console.log(`switched to ${resourceType}`);
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response => response.json())
+    .then(json => setItems(json));
+  }, [resourceType])
+
+
+  const [name, setName] = useState("");
+  const inputRef = useRef();
+
+  function focus() {
+    inputRef.current.focus()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
+        <div>My name is {name}</div>
+        <button onClick={focus}>Focus</button>
+
+        <br />
+        <br />
+
+        <button onClick={decrementCount}>-</button>
+        <span>{state.count}</span>
+        <button onClick={increaseCount}>+</button>
+
+        <br />
+        <br />
+
+        <button onClick={() => setResourceType("posts")}>posts</button>
+        <button onClick={() => setResourceType("users")}>users</button>
+        <button onClick={() => setResourceType("comments")}>comments</button>
+        <h1>{resourceType}</h1>
+        {items.map(item => {
+          return (<pre>{JSON.stringify(item)}</pre>)
+        })}
+    </>
   );
 }
 
